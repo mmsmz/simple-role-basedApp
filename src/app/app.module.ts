49625ffd4050b77a31b977f94a4f6c0c17f01d80
mohamed <marafath07@gmail.com>
2021-03-25 23:51:26 +0530

    adding the files

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    MenuComponent,
+    LoginComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../app.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+ 
+  isLoggedIn = false;
+  getLocatStorageItem: any;
+  adminMenu:boolean =false;
+  userMenu:boolean =false;
+
+
+  constructor(private router: Router,
+    private authenticationService: AppService ) { }
+
+
+  ngOnInit(): void {
+    this.checkingUserLogin();
+    this.getLocatStorageItem = localStorage.getItem('LoggedInUser');
+  }
+
+  checkingUserLogin(){
+    this.getLocatStorageItem = localStorage.getItem('LoggedInUser');
+    debugger
+    if(this.getLocatStorageItem == "admin" ){
+      
+        this.adminMenu = true;
+        this.userMenu = false;
+    }
+    else if(this.getLocatStorageItem == "user" ){
+      this.adminMenu =false;
+      this.userMenu = true;
+    }
+  }
+
+  handleAdminStudent(){
+    this.router.navigateByUrl('/admin');
+  }
+
+  handleStudent(){
+    this.router.navigateByUrl('/user');
+  }
+
+  handleLogout() {
+    this.authenticationService.logout();
+    
+    // this.router.navigateByUrl('/login');
+  }
+}
